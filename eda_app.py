848b9e34b98110e27214dd769710c8f6f65db443
jaechang3456 <78472987+jaechang3456@users.noreply.github.com>
2021-03-11 11:32:30 +0900

    자동차 구매가격 예측

--- eda_app.py
+++ eda_app.py
@@ -52,3 +52,28 @@ def run_eda_app() :
 
     else :
         st.write('선택한 컬럼이 없습니다.')
+
+    # 컬럼을 하나만 선택하면, 해당 컬럼의 최대,최소값에 해당하는 사람의 데이터를 화면에 보여주는 기능
+
+    number_columns = corr_col_list
+    selected_col = st.selectbox('최대,최소값 확인할 컬럼 선택',number_columns)
+
+    min_data = car_df[selected_col].min() == car_df[selected_col]
+    st.write('최소값 데이터')
+    st.dataframe(car_df.loc[ min_data, ] )
+
+    max_data = car_df[selected_col].max() == car_df[selected_col]
+    st.write('최대값 데이터')
+    st.dataframe(car_df.loc[ max_data, ] )
+
+    # 고객이름을 검색할 수 있는 기능 개발
+    # 1. 유저한테 검색어를 받자
+    name = st.text_input('고객의 이름을 입력하세요.')
+    # 2. 검색어를 데이터프레임 커스터머네임 컬럼에 검색하자.
+    search_name = car_df.loc[car_df['Customer Name'].str.contains(name, case=False) , ]
+    # 3. 화면에 결과를 보여주자.
+    if len(search_name) != 0 :
+        st.dataframe(search_name)
+    else :
+        st.write('존재하지 않는 고객입니다.')
+    
